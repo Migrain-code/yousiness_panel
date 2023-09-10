8a4e7bd3bc71dcfee42a462552a723a9bda236a8
migrain.code <migrain.code@gmail.com>
2023-09-10 18:34:55 +0300

    takvim eklentisi test 2

--- app/Services/GoogleCalender.php
+++ app/Services/GoogleCalender.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Services;
+
 use Google\Client;
 use Google\Service\Calendar;
 use Google\Service\Calendar\Event;
@@ -12,9 +14,25 @@ class GoogleCalender
     public function __construct()
     {
         // Google Client oluşturma ve kimlik doğrulama ayarları
-        $this->client = new Client();
-
-        $this->client->setAuthConfig('https://panel.yousiness.com/storage/google/client_secret_449337264437.json');
+        $this->client = new Client([
+            'client_id' => env("GOOGLE_CLIENT_ID"),
+            'client_secret' => env("GOOGLE_CLIENT_SECRET"),
+            'redirect_uri' => env("GOOGLE_REDIRECT_URI"),
+        ]);
+        $this->client->setAuthConfig([
+            "web" => [
+                "client_id" => "449337264437-njtrb3c4t391i80dp0c72pm0sh88970l.apps.googleusercontent.com",
+                "project_id" => "calendertest-398614",
+                "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
+                "token_uri" => "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret" => "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",
+                "javascript_origins" => [
+                    "http://127.0.0.1:8001",
+                    "https://panel.yousiness.com"
+                ]
+            ]
+        ]);
         $this->client->setScopes([Calendar::CALENDAR_EVENTS]);
 
         // Google Service oluşturma
