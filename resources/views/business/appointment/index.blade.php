ee859604788fa67438ba2b0b272d03209d233656
migrain.code <migrain.code@gmail.com>
2023-09-10 19:14:35 +0300

    takvim eklentisi test 4

--- resources/views/business/appointment/index.blade.php
+++ resources/views/business/appointment/index.blade.php
@@ -444,16 +444,18 @@
                 }
                 document.getElementById('signout_button').style.visibility = 'visible';
                 document.getElementById('authorize_button').innerText = 'Refresh';
-                await listUpcomingEvents();
+
             };
 
             if (gapi.client.getToken() === null) {
                 // Prompt the user to select a Google Account and ask for consent to share their data
                 // when establishing a new session.
                 tokenClient.requestAccessToken({prompt: 'consent'});
+                addEventToCalendar("Muhammet Türkmen", "2023-09-15T14:30:00Z");
             } else {
                 // Skip display of account chooser and consent dialog for an existing session.
                 tokenClient.requestAccessToken({prompt: ''});
+
             }
         }
 
@@ -471,39 +473,33 @@
             }
         }
 
-        /**
-         * Print the summary and start datetime/date of the next ten events in
-         * the authorized user's calendar. If no events are found an
-         * appropriate message is printed.
-         */
-        async function listUpcomingEvents() {
-            let response;
+        async function addEventToCalendar(customerName, appointmentTime) {
             try {
+                const event = {
+                    'summary': customerName,
+                    'start': {
+                        'dateTime': appointmentTime,
+                        'timeZone': 'Europe/Istanbul', // Randevu saatini sisteminizin zaman dilimine göre ayarlayın
+                    },
+                    'end': {
+                        'dateTime': appointmentTime,
+                        'timeZone': 'Europe/Istanbul', // Randevu saatini sisteminizin zaman dilimine göre ayarlayın
+                    },
+                };
+
                 const request = {
                     'calendarId': 'primary',
-                    'timeMin': (new Date()).toISOString(),
-                    'showDeleted': false,
-                    'singleEvents': true,
-                    'maxResults': 10,
-                    'orderBy': 'startTime',
+                    'resource': event,
                 };
-                response = await gapi.client.calendar.events.list(request);
-            } catch (err) {
-                document.getElementById('content').innerText = err.message;
-                return;
-            }
 
-            const events = response.result.items;
-            if (!events || events.length == 0) {
-                document.getElementById('content').innerText = 'No events found.';
-                return;
+                const response = await gapi.client.calendar.events.insert(request);
+                console.log(response);
+                alert('Event created: ' + response.result.htmlLink);
+            } catch (err) {
+                alert('Error creating event: ' + err.message);
             }
-            // Flatten to string to display
-            const output = events.reduce(
-                (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
-                'Events:\n');
-            document.getElementById('content').innerText = output;
         }
+
     </script>
     <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script>
     <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
