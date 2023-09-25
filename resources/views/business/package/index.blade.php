65c5bfa41225edee704e7a8f8f40e0a00f8e1d57
migrain.code <migrain.code@gmail.com>
2023-09-25 20:04:16 +0300

    -- index.blade.php paket toplam fiyat güncelledni

--- resources/views/business/package/index.blade.php
+++ resources/views/business/package/index.blade.php
@@ -289,7 +289,7 @@
                             </div>
                             <div class="mb-3 col-md-6">
                                 <label class="form-label">Toplam Tutar</label>
-                                <input type="text" class="form-control" name="total">
+                                <input type="text" class="form-control" id="totalInput" name="total">
                             </div>
                         </div>
                         <div class="row">
@@ -519,5 +519,13 @@
                 }
             })
         }
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+
+        $(function() {
+            $('#totalInput').mask("#.##0", {reverse: true});
+        });
     </script>
 @endsection
