d66d617a7bb46f4678e7f0640cc3cd72f4dda96e
migrain.code <migrain.code@gmail.com>
2023-09-25 19:54:00 +0300

    -- fiyat alanı maskesi eklendi

--- resources/views/business/sales/index.blade.php
+++ resources/views/business/sales/index.blade.php
@@ -224,4 +224,12 @@
            })
        }
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+
+        $(function() {
+            $('#price').mask("#.##0", {reverse: true});
+        });
+    </script>
 @endsection
