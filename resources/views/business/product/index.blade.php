d1b76cd334fa0b1016448657e4afe8b4b5661d8b
migrain.code <migrain.code@gmail.com>
2023-09-25 20:10:59 +0300

    -- fiyat alanları güncelledni

--- resources/views/business/product/index.blade.php
+++ resources/views/business/product/index.blade.php
@@ -166,9 +166,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script>
-
         $(function() {
-            $('#priceInput').mask("#.##0", {reverse: true});
+            $('#priceInput').mask("#.##0,00", {reverse: true});
         });
     </script>
 
