1a86ae2dc4ad0200cc46055d87d9c28523a1f753
migrain.code <migrain.code@gmail.com>
2023-10-01 18:27:32 +0300

    -- ödeme middleware eklendi

--- app/Http/Middleware/SetupMiddleware.php
+++ app/Http/Middleware/SetupMiddleware.php
@@ -16,7 +16,7 @@ class SetupMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->routeIs('business.setup.*')) {
+        if ($request->routeIs('business.setup.*') || $request->route('business.payment.*')) {
             return $next($request);
         }
 
