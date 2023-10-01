cdf882b0fb163acba37c5b3b0b8e95e2ceb5335c
migrain.code <migrain.code@gmail.com>
2023-10-01 19:47:44 +0300

    -- ödeme paypal control

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -175,7 +175,7 @@ class PaymentController extends Controller
             'cancelUrl' => route('business.setup.step4'),
             'custom' => $packet->slug,
         ))->send();
-        dd($response->getData()->id);
+        dd($response->getData());
         $paypal = new BussinessPackagePaypalSaller();
         $paypal->business_id = auth('business')->id();
         $paypal->payment_id = $response["data"]["id"];
