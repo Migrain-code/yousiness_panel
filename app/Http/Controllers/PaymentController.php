d86fd19caf55a5ddd15180de7d96201cb34e4da3
migrain.code <migrain.code@gmail.com>
2023-10-05 23:07:58 +0300

    -- stripe.js test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,6 +8,7 @@ use App\Models\BussinessPackagePaypalSaller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Omnipay\Omnipay;
+use Stripe\Stripe;
 
 class PaymentController extends Controller
 {
@@ -20,97 +21,24 @@ class PaymentController extends Controller
     public function pay(Request $request)
     {
         $parts = explode('/', $request->expiry);
-        $monthPart = $parts[0];
-        $yearPart = $parts[1];
+
         $holder_name = $request->holder_name;
-        $card_number = $request->card_number;
-        $month = $monthPart;
-        $year = $yearPart;
+        $cardNumber = $request->card_number;
+        $expMonth = $parts[0];
+        $expYear = $parts[1];
         $cvc = $request->cvc;
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+        $stripe = new \Stripe\StripeClient(env('STRIPE_KEY'));
+        $token = \Stripe\Token::create([
+            'card' => [
+                'number' => $cardNumber,
+                'exp_month' => $expMonth,
+                'exp_year' => $expYear,
+                'cvc' => $cvc,
+            ],
+        ]);
 
-        // örnek olarak karttan 1$ çekeceğiz. stripe dokümanında belirtildiği üzere karttan çekilecek değeri 100 ile çarpmamız gerekiyor.
-        $currency = 'USD';
-        $price = 1 * 100;
-
-        // stripe secret_key bilgisini config'den alıyoruz.
-        $stripe = new \Stripe\StripeClient(config('stripe.secret_key'));
-
-        try {
-            // kart bilgilerini kullanarak stripe tarafında bir token elde ediyoruz.
-            $stripeToken = $stripe->tokens->create([
-                'card' => [
-                    'number' => $card_number,
-                    'exp_month' => $month,
-                    'exp_year' => $year,
-                    'cvc' => $cvc
-                ]
-            ]);
-
-            // müşteri adını ve kart bilgisini stripe tarafında kaydediyoruz.
-            $customer = $stripe->customers->create([
-                'name' => $holder_name,
-                'source' => $stripeToken['id']
-            ]);
-
-            // stripe tarafında bir setupIntent oluşturuyoruz.
-            // setupIntent işlemi sonucunda stripe, girilen kartın 3ds gerektirip gerektirmediğiyle ilgili bir doğrulama yapıyor.
-            // stripe'ın ödeme sonucunu iletmesi için 'return_url' parametresini belirtiyoruz.
-            // 3ds ekranı sonrası para çekme işleminde kullanmak için 'metadata' kısmında fiyat ve para birimi değerlerini gönderiyoruz.
-            $setupIntent = $stripe->setupIntents->create([
-                'customer' => $customer['id'],
-                'description' => 'test',
-                'payment_method' => $stripeToken['card']['id'],
-                'payment_method_types' => ['card'],
-                'payment_method_options' => [
-                    'card' => [
-                        'request_three_d_secure' => 'any'
-                    ]
-                ],
-                'confirm' => true,
-                'return_url' => config('stripe.merchant_url') .'/stripe-3ds-result',
-                'metadata' => [
-                    'price' => $price,
-                    'currency' => $currency
-                ]
-            ]);
-        }
-            // hata oluşması durumunda hata mesajını json olarak ekrana yazdırıyoruz.
-        catch (\Stripe\Exception\ApiErrorException $e) {
-            return response()->json($e->getMessage(), 500);
-        }
-
-        // eğer doğrulama sonucunda ödeme normal (3ds olmadan) gerçekleşecekse direkt olarak para çekme işlemini yapıyoruz.
-        if ($setupIntent['status'] == 'succeeded') {
-            try {
-                $charge = $stripe->charges->create([
-                    'customer' => $setupIntent['customer'],
-                    'amount' => $price,
-                    'currency' => $currency,
-                    'description' => 'test',
-                    'source' => $stripeToken['card']['id']
-                ]);
-            }
-                // hata oluşması durumunda hata mesajını json olarak ekrana yazdırıyoruz.
-            catch (\Stripe\Exception\ApiErrorException $e) {
-                return response()->json($e->getMessage(), 500);
-            }
-
-            // para çekme işleminde bir sorun olursa ekrana hata yazdırıyoruz.
-            if ($charge['status'] != 'succeeded') {
-                return response()->json('Payment error!', 500);
-            } else {
-                // para çekme işlemi başarılı sonuçlanırsa stripe'dan dönen veriyi ekrana basıyoruz.
-                return response()->json($charge, 200);
-            }
-        }
-
-        // eğer doğrulama sonucunda ödeme 3ds ile gerçekleşecekse kullanıcıyı stripe'ın gönderdiği 3ds ekranına yönlendiriyoruz.
-        if ($setupIntent['status'] == 'requires_action') {
-            return Redirect::to($setupIntent['next_action']['redirect_to_url']['url']);
-        } else {
-            // setupIntent işleminde bir sorun olursa ekrana hata yazdırıyoruz.
-            return response()->json('3ds payment error!', 500);
-        }
+        dd($token);
     }
 
     public function stripe3dsResult(Request $request)
--- resources/views/business/setup/payment/form.blade.php
+++ resources/views/business/setup/payment/form.blade.php
@@ -52,7 +52,7 @@
             display: none;
         }
     </style>
-
+    <script src="https://js.stripe.com/v3/"></script>
 @endsection
 @section('content')
     @include('layouts.component.error')
@@ -179,47 +179,22 @@
                                     @csrf
                                     <input type="hidden" name="package_id" value="{{$package->id}}">
                                     <!-- Müşteri Bilgileri -->
-                                    <div class="form-group">
-                                        <label for="name">Kart Sahibinin Adı</label>
-                                        <input type="text" id="name" name="holder_name" value="{{old("name")}}" class="form-control" required>
-                                    </div>
 
-                                    <div class="form-group">
-                                        <label for="email">E-posta</label>
-                                        <input type="email" id="email" name="email" value="{{old("email")}}" class="form-control" required>
-                                    </div>
-                                    <!-- Kart Bilgileri -->
-                                    <div class="form-group">
-                                        <label for="card-number">Kredi Kartı Numarası</label>
-                                        <input type="text" id="card-number" name="card_number" value="{{old("number")}}" class="form-control" required>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="expiry">Son Kullanma Tarihi (MM/YY)</label>
-                                        <input type="text" id="expiry" name="expiry" value="{{old("expiry")}}" class="form-control" required>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="cvc">CVC</label>
-                                        <input type="text" id="cvc" name="cvc" value="{{old("cvc")}}" class="form-control" required>
-                                    </div>
                                     <div class="onboarding-btn">
                                         <a onclick="$('#step5Form').submit()" href="#" style="color: white">Ödeme Yap</a>
                                     </div>
                                 </form>
                             </div>
                             <div id="noOnlinePayment">
-                                <form method="post" id="step5Form" action="{{route('business.payment.pay')}}">
-                                    <div class="alert alert-warning">
-                                        <i class="fa fa-info-circle"></i> <b>5646546546546546456564</b> nolu adrese <b>{{$package->price}} €</b> paket ödemesi yapıp dekontu buraya yüklemeniz gerekmektedir. Dekontunuz yükleyip gönderdikten sonra kontrol edilip onaylandıktan sonra erişiminiz açılacaktır
+                                <form id="payment-form">
+                                    <div id="card-element">
+                                        <!-- Elements will create input elements here -->
                                     </div>
-                                    <div class="form-group">
-                                        <label for="name">Dekont</label>
-                                        <input type="file" id="name" name="file" class="form-control" required>
-                                    </div>
-                                    <div class="onboarding-btn">
-                                        <a type="submit" style="color: white">Ödeme Yap</a>
-                                    </div>
+
+                                    <!-- We'll put the error messages in this element -->
+                                    <div id="card-errors" role="alert"></div>
+
+                                    <button id="submit">Submit Payment</button>
                                 </form>
                             </div>
                             <div id="paymentSummary" style="font-size: 18px;text-align: right;">
@@ -300,5 +275,55 @@
             });
         });
     </script>
+    <script>
+        var stripe = Stripe('pk_test_51NvSDhIHb2EidFuB3LbbZHqZbywNWZbvQNsyDop4mHT1OzxOpax5uotEqlToQKrawEAJMH5OXa4JR1FrE3OBD7cC00KngyS4JA');
+        var elements = stripe.elements();
 
+        var style = {
+            base: {
+                color: "#32325d",
+            }
+        };
+
+        var card = elements.create("card", { style: style });
+        card.mount("#card-element");
+
+        card.on('change', ({error}) => {
+            let displayError = document.getElementById('card-errors');
+            if (error) {
+                displayError.textContent = error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+        var form = document.getElementById('payment-form');
+
+        form.addEventListener('submit', function(ev) {
+            ev.preventDefault();
+            // If the client secret was rendered server-side as a data-secret attribute
+            // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: 'Jenny Rosen'
+                    }
+                }
+            }).then(function(result) {
+                if (result.error) {
+                    // Show error to your customer (for example, insufficient funds)
+                    console.log(result.error.message);
+                } else {
+                    // The payment has been processed!
+                    if (result.paymentIntent.status === 'succeeded') {
+                        // Show a success message to your customer
+                        // There's a risk of the customer closing the window before callback
+                        // execution. Set up a webhook or plugin to listen for the
+                        // payment_intent.succeeded event that handles any business critical
+                        // post-payment actions.
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
