8fe305240526c303fdac0642d86a247ad92f849d
migrain.code <migrain.code@gmail.com>
2023-10-15 14:49:22 +0300

    -- işletme durm metni düzeltildi

--- app/Http/Controllers/Business/CustomerController.php
+++ app/Http/Controllers/Business/CustomerController.php
@@ -111,7 +111,7 @@ class CustomerController extends Controller
      */
     public function edit(Customer $customer)
     {
-        dd($customer->permissions);
+        dd($customer);
         return view('business.customer.edit', compact('customer'));
     }
 
