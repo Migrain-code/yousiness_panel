873fcd17b91b176effc4b91916a64b2896b353d9
migrain.code <migrain.code@gmail.com>
2023-11-13 01:34:40 +0300

    $session test

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,15 +1,60 @@
-<!doctype html>
-<html lang="tr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ödeme Başarılı</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .payment-success {
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            text-align: center;
+            width: 80%;
+            max-width: 400px;
+            box-sizing: border-box;
+        }
+
+        .success-icon {
+            color: #4CAF50;
+            font-size: 48px;
+            margin-bottom: 20px;
+        }
+
+        h1 {
+            color: #333;
+            margin-bottom: 10px;
+        }
+
+        p {
+            color: #666;
+            margin-bottom: 20px;
+        }
+
+        a {
+            color: #3498db;
+            text-decoration: none;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-    <div style="position: absolute; top: 30%; left: 50%; transform: translate(-30%, -50%)">
-        <img src="/business/success.png" style="width: 500px;height: 500px">
-    </div>
+<div class="payment-success">
+    <div class="success-icon"><i class="fas fa-check-circle"></i></div>
+    <h1>Ödeme Başarılı!</h1>
+    <p>Teşekkür ederiz, ödemeniz başarıyla tamamlandı.</p>
+</div>
 </body>
 </html>
