7ad3db87ef126c29067f04fd1dfd145d30a11da5
migrain.code <migrain.code@gmail.com>
2023-11-18 20:21:25 +0300

    setup 1 text güncellendi

--- resources/views/business/setup/step-1.blade.php
+++ resources/views/business/setup/step-1.blade.php
@@ -23,7 +23,7 @@
         <div class="onboarding-content-box content-wrap">
             <div class="onborad-set">
                 <div class="onboarding-title">
-                    <h2>Was ist Salon Kategorien?<span>*</span></h2>
+                    <h2>Was bedeutet Salon Kategorie?<span>*</span></h2>
                     <h6>Wir werden ihren Salon in Kategorien einteilen. Deshalb ist es wichtig, dass sie die entsprechende Salon Arten auswählen.</h6>
                 </div>
                 <style>
