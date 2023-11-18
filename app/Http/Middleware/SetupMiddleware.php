453294a911d18855104a299817451f7bf8235e2e
migrain.code <migrain.code@gmail.com>
2023-11-18 11:46:42 +0300

    calender.js responsive edildi

--- app/Http/Middleware/SetupMiddleware.php
+++ app/Http/Middleware/SetupMiddleware.php
@@ -16,19 +16,32 @@ class SetupMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (($request->routeIs('business.setup.*') || $request->routeIs('business.payment.*')) && auth('business')->check()) {
-            // Kullanıcı setup veya payment rotasına gidiyorsa ve giriş yapmışsa
-            if (auth('business')->user()->is_setup == 0) {
-                return redirect()->route('business.setup.step1');
-            } elseif (auth('business')->user()->is_setup == 1) {
-                // Eğer kullanıcının is_setup değeri 1 ise, erişimi engelle
-                return redirect()->route('business.home');
-            }
-            // Eğer is_setup değeri 0 değilse veya kullanıcı giriş yapmamışsa, normal işleyiş devam etsin
-            return $next($request);
-        }
+        if (auth('business')->check()) {
+            $user = auth('business')->user();
 
-        // Yukarıdaki koşullar sağlanmazsa, normal işleyiş devam etsin
-        return $next($request);
+            if ($user->is_setup == 0) {
+                // Kullanıcı setup yapılmamışsa
+                if ($request->routeIs('business.setup.*') || $request->routeIs('business.payment.*')) {
+                    // Kullanıcı setup sayfalarına veya ödeme sayfalarına erişmeye çalışıyorsa
+                    return $next($request);
+                } else {
+                    // Kullanıcı setup yapmamışsa ve diğer sayfalara erişmeye çalışıyorsa
+                    return redirect()->route('business.setup.step1');
+                }
+            } else {
+                // Kullanıcı setup yapmışsa
+                if ($request->routeIs('business.setup.*') || $request->routeIs('business.payment.*')) {
+                    // Kullanıcı setup sayfalarına veya ödeme sayfalarına erişmeye çalışıyorsa
+                    return redirect()->route('business.home');
+                } else {
+                    // Kullanıcı setup yapmışsa ve diğer sayfalara erişmeye çalışıyorsa
+                    return $next($request);
+                }
+            }
+        } else {
+            // Kullanıcı giriş yapmamışsa
+            // İsterseniz bu durumda giriş yapma sayfasına yönlendirme yapabilirsiniz
+            return redirect()->route('business.login');
+        }
     }
 }
