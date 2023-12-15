b3eee51a56cb01020fc33b9939fc39c3f16829ab
migrain.code <migrain.code@gmail.com>
2023-12-15 16:51:03 +0300

    salon info butonlar düzeltildi

--- resources/views/business/personel/profile/notification.blade.php
+++ resources/views/business/personel/profile/notification.blade.php
@@ -3,24 +3,24 @@
         <form class="modal-content" method="post" action="{{route('business.sendNotification')}}">
             @csrf
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Bildirim Oluştur</h5>
+                <h5 class="modal-title" id="exampleModalLabel">NACHRICHT ERSTELLEN</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <label for="basic-url" class="form-label d-block mt-2">Başlık</label>
-                <input type="text" name="title" class="form-control w-100" placeholder="Bildirim Başlığı">
+                <label for="basic-url" class="form-label d-block mt-2">Überschrift</label>
+                <input type="text" name="title" class="form-control w-100" placeholder="">
 
-                <label for="basic-url" class="form-label d-block mt-2">İçerik</label>
-                <input type="text" name="message" class="form-control w-100" placeholder="Bildirim İçeriği">
+                <label for="basic-url" class="form-label d-block mt-2">Inhalt</label>
+                <input type="text" name="message" class="form-control w-100" placeholder="">
 
                 <label for="basic-url" class="form-label d-block mt-2">Link</label>
-                <input type="text" name="link" class="form-control w-100" placeholder="Bildirim Linki">
+                <input type="text" name="link" class="form-control w-100" placeholder="">
                 <input type="hidden" name="personel_id" value="{{$personel->id}}">
 
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Abbrechen</button>
-                <button type="submit" class="btn btn-primary">Speichern</button>
+                <button type="submit" class="btn btn-primary">Senden</button>
             </div>
         </form>
     </div>
