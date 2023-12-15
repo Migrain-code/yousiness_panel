4593faed689ceb0eb35ae24a244f5004d4323e54
migrain.code <migrain.code@gmail.com>
2023-12-15 18:43:39 +0300

    salon info butonlar düzeltildi

--- resources/views/business/appointment/list.blade.php
+++ resources/views/business/appointment/list.blade.php
@@ -209,7 +209,7 @@
                                                             <div class="d-flex">
                                                                 @forelse($appointment->services as $service)
                                                                     <label class="form-label">
-                                                                        <button type="button" class="" style="width: 19px;background: none;border: none;font-size: 10px;border-radius: 50%;color: #01a3ff;padding: 2px;" data-toggle="tooltip" data-placement="top" title="{{$service->personel->name}}">
+                                                                        <button type="button" class="" style="width: 30px;background: none;border: none;font-size: 10px;border-radius: 50%;color: #01a3ff;padding: 2px;" data-toggle="tooltip" data-placement="top" title="{{$service->personel->name}}">
                                                                             <img src="{{asset($service->personel->image)}}" style="object-fit: cover;height: 45px;width: 45px" class="rounded-circle">
                                                                         </button>
                                                                     </label>
