24b6d4a7e2548f19cd7a4f5125b2be27ea303983
migrain.code <migrain.code@gmail.com>
2023-12-15 16:18:18 +0300

    salon info butonlar düzeltildi

--- resources/views/business/service/index.blade.php
+++ resources/views/business/service/index.blade.php
@@ -5,7 +5,7 @@
             <div class="page-titles style1">
                 <div class="d-flex align-items-center">
                     <h2 class="heading">
-                        Dienstleistungen
+                        DIENSTLEISTUNGEN
                     </h2>
                 </div>
                 <div id="datepicker" class="input-group date dz-calender" data-date-format="mm-dd-yyyy">
