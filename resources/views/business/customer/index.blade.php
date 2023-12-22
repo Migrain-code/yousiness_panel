b0d9ca86cadf68041a2ce93a550c526e281f0204
migrain.code <migrain.code@gmail.com>
2023-12-22 08:09:42 +0300

    müşteri sayfası fixlendi

--- resources/views/business/customer/index.blade.php
+++ resources/views/business/customer/index.blade.php
@@ -162,7 +162,6 @@
     </div>
 @endsection
 @section('scripts')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.1.60/inputmask/jquery.inputmask.js"></script>
 
     <script>
