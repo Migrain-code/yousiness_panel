34335c14f6c5089a9f8a3646a1d7ef0f3ad9a39d
migrain.code <migrain.code@gmail.com>
2023-12-22 08:01:09 +0300

    müşteri sayfası fixlendi

--- resources/views/business/customer/index.blade.php
+++ resources/views/business/customer/index.blade.php
@@ -133,7 +133,7 @@
                        </div>
                         <div class="form-group">
                             <label>Telefon</label>
-                            <input type="text" class="form-control" name="email">
+                            <input type="text" class="form-control" name="email" id="phone">
                         </div>
                         <div class="form-group">
                             <label>E-Mail</label>
@@ -221,5 +221,34 @@
         $('#typeSelect').on('change', function (){
             $('#typeForm').submit();
         });
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.1.60/inputmask/jquery.inputmask.js"></script>
+
+    <script>
+        const input = document.querySelector("#phone");
+        const iti = window.intlTelInput(input, {
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+            customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
+                updatePlaceholder(selectedCountryPlaceholder);
+                return selectedCountryPlaceholder;
+            },
+        });
+
+        // Örnek olarak: Numarayı uluslararası formatta alma
+        function getNumber() {
+            return iti.getNumber();
+        }
+
+        input.addEventListener('countrychange', function () {
+            $("#phone").val("");
+        });
+
+        function updatePlaceholder(originalData) {
+            let mask = "";
+            mask = originalData.replace(/[0-9]/g, "9");
+
+            $("#phone").inputmask({"mask": mask});
+        }
     </script>
 @endsection
