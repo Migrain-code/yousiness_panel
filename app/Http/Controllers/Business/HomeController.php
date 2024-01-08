9b219ae4ff6a85c8156f88cec172cbccd11454ce
migrain.code <migrain.code@gmail.com>
2024-01-08 16:47:46 +0300

    anasayfa düzeltildi

--- app/Http/Controllers/Business/HomeController.php
+++ app/Http/Controllers/Business/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
                 ->latest()
                 ->get();
 
-            $appointments = auth('business')->user()->appointments()->where('status', 7)->get();
+            $appointments = auth('business')->user()->appointments()->where('status', 4)->get();
             $totalAppointments = auth('business')->user()->appointments()->count();
             foreach ($appointments as $row){
                     $earning+=$row->calculateTotal($row->services);
